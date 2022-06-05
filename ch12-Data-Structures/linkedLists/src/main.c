d4de40500c61c69fcf7221c79cf7255577e72081
Patrick Mims <patrickkevinmims@gmail.com>
2022-06-04 21:57:47 -0700

    First Commit

---
+++ ch12-Data-Structures/linkedLists/src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+int main(int argc, char **argv)
+{
+  struct node *head = NULL;
+
+  if((head = malloc(sizeof(struct node))) == NULL)
+  {
+    printf("No more memory\n");
+    exit(EXIT_FAILURE);
+  }
+
+  insert_node(&head, 5);
+  insert_node(&head, 10);
+  insert_node(&head, 15);
+
+  display(&head);
+
+  struct node *i = delete_node(head, 10);
+
+  display(&head);
+
+  struct node *j = delete_node(head, 5);
+
+  display(&head);
+
+  return 0;
+}
+
+/* A linked list is a linear collection of self-referential structures, called nodes */
